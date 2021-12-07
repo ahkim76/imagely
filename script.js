4921cacaf15415e22c0047a5d4f153661e1bbe2b
Alex Kim <88253088+ahkim76@users.noreply.github.com>
2021-12-07 17:15:00 -0500

    Commit script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+//SMOOTH SCROLLING
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    console.log(href);
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    //scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+    //close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.ELEMENT_NODE.classList.toggle("nav-open");
+    }
+  });
+});
+
+// MOBILE BUTTON NAVIGATION THINGY
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
